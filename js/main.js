4943a7d80d99ae21b4fae38ae55bb0e3af42633a
Vladislav <zhurkin111@bk.ru>
2021-02-02 16:07:45 +0300

    refact function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
 'use strict';
 
-function getNumberRandom(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomArbitary(min, max) {
+  if (min === max) {
+    return 0;
+  }
+
+  const minNum = Math.min(min,max);
+  const maxNum = Math.max(max,min);
+
+  return Math.random() * (maxNum - minNum + 1) + minNum;
 }
 
-function getNumberRandomOfSigns(min, max, numberOfSigns){
-  min = Math.ceil(min);
-  max = Math.floor(max);
+getRandomArbitary(1, 6);
 
-  let number = Math.random() * (max - min + 1) + min;
+// console.log(getRandomArbitary(1, 6));
 
-  let transformOfSigns = number.toFixed(numberOfSigns);
-
-  return Number(transformOfSigns);
+function getRandomInt(min, max) {
+  return Math.floor(getRandomArbitary(min, max));
 }
 
-getNumberRandom(1, 6);
+getRandomInt(1, 6);
 
-getNumberRandomOfSigns(1, 6, 2);
+// console.log(getRandomInt(1, 6));
+
+function getRandomToFixed(min, max, numberOfSigns) {
+  let number = getRandomArbitary(min, max);
+
+  return +number.toFixed(numberOfSigns)
+}
+
+getRandomToFixed(1, 6);
+
+// console.log(getRandomToFixed(6, 1, 5));
